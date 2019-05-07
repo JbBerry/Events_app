6b34c754763e7b9baa7be161a924d80b6aab6c01
Euan Gilmour <a9404253@9404253s-MacBook-Pro.local>
2019-05-07 14:03:32 +0100

    Files set up

---
+++ src/components/EventsForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class EventsForm extends Component {
+  constructor() {
+
+  }
+}
+
+export default EventsForm;
---
+++ src/containers/EventsBox.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class EventsBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  };
+
+  render() {
+    return(
+      <div className='events-box'>
+
+      </div>
+    );
+  };
+};
+
+export default EventsBox
